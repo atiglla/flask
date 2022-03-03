bdc6fe0b50914eea5d47d4f7ae760dc0898d1035
atiglla <99499241+atiglla@users.noreply.github.com>
2022-03-03 12:40:24 -0500

    second commit

--- server.py
+++ server.py
@@ -12,13 +12,14 @@ def dojo():
 
 @app.route ("/say/<usuario>")
 def usuario (usuario):
-    if type(usuario)==str:
-        return "¡Hola, "+usuario+"!"  
+        if type(usuario)==str:  
+                return "¡Hola, "+(usuario.capitalize())+"!"  
 
-@app.route ("/repeat/<numero>/<palabra>")
+@app.route ("/repeat/<int:numero>/<palabra>")
 def repetir (numero,palabra):
     if (type(numero)==int and type(palabra)==str):
         return (palabra+" ")*int(numero)
+    
 
 
 if __name__=="__main__":#Asegurando de que el archivo se esta ejecutando directamente y no desde otro modulo
